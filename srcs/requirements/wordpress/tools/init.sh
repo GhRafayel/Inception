b903b7c19a5e9a9b41c01cd2e6f92ab40a0b7fad
GhRafayel <ghazarysnrafayel@gmail.com>
2026-02-16 21:26:12 +0400

    Mon Feb 16 21:26 at home

---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+cd /var/www/html
+
+# Install WordPress only if not installed yet
+if [ ! -f wp-config.php ]; then
+    echo "Installing WordPress..."
+    wget https://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz --strip-components=1
+    rm -f latest.tar.gz
+fi
+
+# Generate wp-config.php from template
+envsubst < /wp-config-template.php > wp-config.php
+
+# Start PHP-FPM as PID 1
+exec php-fpm7.4 -F
